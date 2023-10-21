dbce70d694a67bb15061183888b0a892d021c289
Ikram Akbar <ikramakbar71.com>
2023-10-21 15:41:16 +0600

    complete the counter

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+//increment button: 
+const countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
+const saveBtn = document.getElementById("save-btn")
+let count = 0;
+
+const increment = () => {
+  
+    count += 1;
+    countEl.innerText = count;
+}
+
+const save = () => {
+  
+    let countStr = count + " __ ";
+    saveEl.innerText += countStr;
+    countEl.innerText = 0;
+    count = 0;
+
+}
